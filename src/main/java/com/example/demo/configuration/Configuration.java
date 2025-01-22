c77c5205d8fdfc4f65c5ae5a3337629e08c179dc
Matheus Monteiro <mathosmathe2002@gmail.com>
2025-01-22 15:53:10 -0300

    refatorar código sem alterar a funcionalidade

--- src/main/java/com/example/demo/configuration/Configuration.java
+++ src/main/java/com/example/demo/configuration/Configuration.java
@@ -8,7 +8,7 @@ public class Configuration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Permite CORS para todas as rotas
-                .allowedOrigins("https://first-project-woad-two.vercel.app", "https://first-project-woad-two.vercel.app/filtro", "http://localhost:4200") // Permite esses domínios
+                .allowedOrigins("https://project-tabela-fipe-woad-two.vercel.app", "https://project-tabela-fipe-woad-two.vercel.app/filtro", "http://localhost:4200") // Permite esses domínios
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
                 .allowedHeaders("*") // Permite todos os cabeçalhos
                 .allowCredentials(true);
